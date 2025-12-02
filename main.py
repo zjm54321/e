2df39ed5b72d336689ce83af72b41b147708e9a0
zjm54321 <zhang_jia_ming@outlook.com>
2025-12-03 04:22:42 +0800

    feat: add steps

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from code!")
+import sys
+import cv2
+import numpy as np
+import os
 
+def log(msg):
+    """输出调试信息到 stderr，不影响 stdout 的控制指令"""
+    sys.stderr.write(f"[Debug] {msg}\n")
+    sys.stderr.flush()
+
+def main():
+    # 启用debug
+    print("debug=true", flush=True)
+
+    log("开始！")
+
+    print("task 1",flush=True)
+    
+    while True:
+        print("LEFT", flush=True)
+            
 
 if __name__ == "__main__":
     main()
